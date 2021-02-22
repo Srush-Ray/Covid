0df7bbd92b999aea90544b9e4fbe1d65c955f067
Srushti <srushti.raybhoge1999@gmail.com>
2021-02-22 09:35:42 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Covid from "./Covid/Covid";
+import {
+  Switch,
+  Route,
+  withRouter,
+  Link,
+  NavLink,
+  BrowserRouter,
+  Router,
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/Covid" component={Covid} />
+          <Route path="/" component={Covid} />
+        </Switch>
+      </BrowserRouter>
       <Covid />
     </div>
   );
